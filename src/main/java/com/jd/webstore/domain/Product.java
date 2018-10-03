13783499821f6a4cf668ffc18d09f06687c8f739
Jakub  Dobrogowski <dobrogowskijakub@gmail.com>
2018-10-03 15:29:37 +0200

    Product class

---
+++ src/main/java/com/jd/webstore/domain/Product.java
@@ -0,0 +1,26 @@
+package com.jd.webstore.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private String productId;
+    private String name;
+    private BigDecimal unitPrice;
+    private String description;
+    private String manufacturer;
+    private String category;
+    private long unitsInStock;
+    private long unitsInOrder;
+    private boolean discontinued;
+    private String condition;
+}
